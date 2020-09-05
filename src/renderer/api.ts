fb885202b4bb5ec4fdb04bd8dfbd32a14758c830
milleniumbug <milleniumbug@protonmail.com>
2020-09-05 17:25:12 +0200

    Provide Mock API for testing independently of the server

--- src/renderer/api.ts
+++ src/renderer/api.ts
@@ -1,3 +1,16 @@
-import { DidacticalEnigmaRestApi } from "../api/src/didacticalEnigmaRestApi";
+import { DidacticalEnigmaRestApiModels } from "../api/src/didacticalEnigmaRestApi";
+//import { DidacticalEnigmaRestApi } from "../api/src/didacticalEnigmaRestApi";
+import { MockDidacticalEnigmaApi } from "./mockApi";
 
-export const api = new DidacticalEnigmaRestApi({ baseUri: "http://localhost:5000" });
+export interface DidacticalEnigmaApi {
+    listDataSources(): Promise<DidacticalEnigmaRestApiModels.ListDataSourcesResponse>;
+    listRadicals(): Promise<DidacticalEnigmaRestApiModels.ListRadicalsResponse>;
+    requestInformationFromDataSources(options: DidacticalEnigmaRestApiModels.DidacticalEnigmaRestApiRequestInformationFromDataSourcesOptionalParams): Promise<DidacticalEnigmaRestApiModels.RequestInformationFromDataSourcesResponse>;
+    selectRadicals(options: DidacticalEnigmaRestApiModels.DidacticalEnigmaRestApiSelectRadicalsOptionalParams): Promise<DidacticalEnigmaRestApiModels.SelectRadicalsResponse>;
+    getWordInformation(options: DidacticalEnigmaRestApiModels.DidacticalEnigmaRestApiGetWordInformationOptionalParams): Promise<DidacticalEnigmaRestApiModels.GetWordInformationResponse>;
+    postText(options: DidacticalEnigmaRestApiModels.DidacticalEnigmaRestApiPostTextOptionalParams): Promise<DidacticalEnigmaRestApiModels.PostTextResponse>;
+    deleteText(options: DidacticalEnigmaRestApiModels.DidacticalEnigmaRestApiDeleteTextOptionalParams): Promise<unknown>;
+}
+
+//export const api : DidacticalEnigmaApi = new DidacticalEnigmaRestApi({ baseUri: "http://localhost:5000" });
+export const api : DidacticalEnigmaApi = new MockDidacticalEnigmaApi;
